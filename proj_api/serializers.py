b274de9b2adcf4e54aa46fdc454a483264244fdb
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2025-01-26 12:09:42 +0000

    Feat: Change genre, role, and instruments from readonly

--- proj_api/serializers.py
+++ proj_api/serializers.py
@@ -6,15 +6,36 @@ import sys
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
-    role = serializers.ReadOnlyField(source='profile.role')
-    genres = serializers.ReadOnlyField(source='profile.genres')
-    instruments = serializers.ReadOnlyField(source='profile.instruments')
+    role = serializers.ChoiceField(
+        choices=[
+            ('basic', 'Basic User'),
+            ('organiser', 'Event Organiser'),
+            ('musician', 'Musician/Band'),
+        ]
+    )
+    genres = serializers.CharField(allow_blank=True, required=False)
+    instruments = serializers.CharField(allow_blank=True, required=False)
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id', 'profile_image', 'role',
             'genres', 'instruments',
         )
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', {})
+        role = profile_data.get('role', instance.profile.role)
+        genres = profile_data.get('genres', instance.profile.genres)
+        instruments = profile_data.get('instruments', instance.profile.instruments)
+
+        # Update the profile fields
+        profile = instance.profile
+        profile.role = role
+        profile.genres = genres
+        profile.instruments = instruments
+        profile.save()
+
+        return super().update(instance, validated_data)
 
 class TokenSerializer(serializers.ModelSerializer):
     """
